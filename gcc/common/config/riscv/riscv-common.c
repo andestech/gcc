70420c7c1052380cc698be94169d3efe8ad38028
Kito Cheng <kito.cheng@gmail.com>
2016-11-22 10:55:58 +0800

    Enable -msave-restore for -Os by default

--- gcc/common/config/riscv/riscv-common.c
+++ gcc/common/config/riscv/riscv-common.c
@@ -655,6 +655,7 @@ static const struct default_options riscv_option_optimization_table[] =
   {
     { OPT_LEVELS_1_PLUS, OPT_fsection_anchors, NULL, 1 },
     { OPT_LEVELS_2_PLUS, OPT_free, NULL, 1 },
+    { OPT_LEVELS_SIZE, OPT_msave_restore, NULL, 1 },
     { OPT_LEVELS_NONE, 0, NULL, 0 }
   };
 
