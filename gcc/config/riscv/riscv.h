16ecde3802dda54cc5bc5080ae343feefb5cb279
Kito Cheng <kito.cheng@gmail.com>
2018-06-21 17:26:34 +0800

    Rename option -m[no-]fma to -m[no-]ext-fma

--- gcc/config/riscv/riscv.h
+++ gcc/config/riscv/riscv.h
@@ -1021,7 +1021,7 @@ extern void riscv_remove_unneeded_save_restore_calls (void);
   CMODEL_SPEC
 
 #define MULTILIB_DEFAULTS \
-  { "mcmodel=medlow", "mfma" }
+  { "mcmodel=medlow", "mext-fpu-fma" }
 
 extern tree riscv_fp16_type_node;
 #endif /* ! GCC_RISCV_H */
