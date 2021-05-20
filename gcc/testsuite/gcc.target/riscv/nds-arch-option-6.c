31a7123814b6f0e9e0b044fe9c7d1621b6343c52
Jet Yang <jet4085@andestech.com>
2021-05-20 23:08:07 +0800

    Add testcases about enabling zfh by -mext-vector

---
+++ gcc/testsuite/gcc.target/riscv/nds-arch-option-6.c
@@ -0,0 +1,6 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32if -mext-vector -mno-ext-dsp" } */
+int foo()
+{
+}
+/* { dg-final { scan-assembler ".attribute arch, \"rv32i2p0_f2p0_v1p0_zfh0p1\"" } } */
