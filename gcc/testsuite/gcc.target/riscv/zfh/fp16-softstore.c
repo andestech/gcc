d8b5ac5e7b0fd206fd33cb2a854268e3081d1591
Jim Lin <jim@andestech.com>
2021-12-13 16:01:00 +0800

    Specify riscv64 required for testcases
    It has error cc1: error: unknown cpu 'n45' for '-mtune' with -mcpu=n45 option

--- gcc/testsuite/gcc.target/riscv/zfh/fp16-softstore.c
+++ gcc/testsuite/gcc.target/riscv/zfh/fp16-softstore.c
@@ -1,6 +1,6 @@
 /* Test that HFmode should locate in define_mode_attr softstore */
 /* { dg-do compile } */
-/* { dg-require-effective-target riscv_hard_fp } */
+/* { dg-require-effective-target riscv64 } */
 /* { dg-options "-O1 -march=rv64ifd -mabi=lp64d -mcmodel=medany -mzfh" } */
 
 __fp16 f;
